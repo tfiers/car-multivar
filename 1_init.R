b77b846025854dabb79e9556a34a12a24d4e5f7f
tfiers <tomas.fiers@gmail.com>
2017-06-04 13:11:25 +0200

    Find & remove outliers; Jittered points plot of noise

--- 1_init.R
+++ 1_init.R
@@ -3,7 +3,7 @@ options(digits = 2) # Set output display precision
 cat("\014") # Clear console
 # Hit Ctrl+Shift+L to clear plots
 
-regenerate_plots = FALSE
+regenerate_plots = TRUE
 
 library(ggplot2)
 library(GGally)
@@ -30,8 +30,35 @@ factor_cols = which(names(X) %in% factor_col_names)
 continuous_cols = which(!(names(X) %in% id_col_names)
                         & !(names(X) %in% factor_col_names))
 
+# One-dimensional jittered points plot of 'noise_level'
 if (regenerate_plots) {
-  ggpairs(XTest[,c(continuous_cols, factor_cols)], aes(alpha=0.4), labeller='label_parsed')
-  ggsave('pairs.pdf', width=15, height=10, scale=1)
+  ggplot(XTrain, aes(x=noise_level, y='noise_level')) +
+    geom_jitter(width=0.4) +
+    theme(axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        legend.position='none')
+  ggsave('jitter_noise.pdf', width=12, height=2)
+}
+
+
+# Find, save, and remove all extreme univariate outliers
+outlier_indexes = c()
+for (col in continuous_cols) {
+  col_data = XTrain[,col]
+  Q1 = quantile(col_data, c(0.25))
+  Q3 = quantile(col_data, c(0.75))
+  IQR = Q3 - Q1
+  new_outliers = which(col_data < Q1 - 15*IQR |
+                      col_data > Q3 + 15*IQR)
+  outlier_indexes = c(outlier_indexes, new_outliers)
+}
+outliers = XTrain[outlier_indexes,]
+XTrain = XTrain[-outlier_indexes,]
+
+# Pairwise plots
+if (regenerate_plots) {
+  ggpairs(XTrain[,c(continuous_cols, factor_cols)], aes(alpha=0.4), labeller='label_parsed')
+  ggsave('pairs.pdf', width=15, height=10)
 }
 
