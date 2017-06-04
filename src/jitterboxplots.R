f848aec92af25a57e9b0a718b3018edb75df7334
tfiers <tomas.fiers@gmail.com>
2017-06-04 21:38:29 +0200

    Update jitterbox-plots

--- src/jitterboxplots.R
+++ src/jitterboxplots.R
@@ -4,15 +4,17 @@ source('init.R')
 
 library(ggplot2)
 
-# Define function
-make_jitterbox_plots = function(col_names, filename_infix='', box=TRUE) {
+# Define plotting function
+make_jitterbox_plots = function(col_names, filename_prefix='jitterbox_', box=TRUE) {
   for (col_name in col_names) {
     col = which(names(X) == col_name)
     df = data.frame(vec=XTrain[,col])
 
     p = ggplot(df, aes(x=1, y=vec, alpha=0.5))
     if (box) {
-      p = p + geom_boxplot(outlier.shape=NA)
+      p = p + geom_boxplot(outlier.shape=NA) + xlim(0.5,1.5)
+    } else {
+      p = p + xlim(0.7,1.3)
     }
     p +
     geom_jitter(width=0.2) +
@@ -24,7 +26,7 @@ make_jitterbox_plots = function(col_names, filename_infix='', box=TRUE) {
           axis.ticks.y=element_blank(),
           legend.position='none')
 
-    filename = paste('../jitterbox_', filename_infix, col_name, '.pdf', sep='')
+    filename = paste('../', filename_prefix, col_name, '.pdf', sep='')
     ggsave(filename, width=15, height=1.5)
   }
 }
