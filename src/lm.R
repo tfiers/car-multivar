35c64b576830cf827fe3b23809550a4bf09f9803
tfiers <tomas.fiers@gmail.com>
2017-06-06 04:45:45 +0200

    Backup dump

--- src/lm.R
+++ src/lm.R
@@ -6,24 +6,15 @@ source('remove_outliers.R')
 library(MASS)
 library(car)
 
-remove_stdres_outliers = FALSE
 transform_predictors = TRUE
-transform_response = FALSE
+remove_stdres_outliers = TRUE
+transform_response = TRUE
 
 # (Extra plot: MD-RD)
 cont_cols = XTrain[,continuous_cols]
 mcd = covMcd(cont_cols)
 rd = sqrt(mahalanobis(cont_cols, mcd$center, mcd$cov))
 md = sqrt(mahalanobis(cont_cols, colMeans(cont_cols), cov(cont_cols)))
-plot(md, rd)
-
-# Define, save and remove lm outliers
-# (These were found with 'which(abs(stdres(lm1)) > 5))')
-if (remove_stdres_outliers) {
-  lm_outliers_idx = c(176, 317, 635)
-  lm_outliers = XTrain[lm_outliers_idx,]
-  XTrain = XTrain[-lm_outliers_idx,]
-}
 
 # Remember: data got shuffled in init.R.
 # First/top number is original row;
@@ -46,7 +37,7 @@ if (transform_predictors) {
     qqline(transf_coldata)
     p_after = shapiro.test(transf_coldata)$p
     #
-    if (p_after > 0.05) {
+    if ((abs(pt$lambda) < 3) && name != 'co2') {
       XTrain[,col] = transf_coldata  
     }
     #
@@ -55,6 +46,15 @@ if (transform_predictors) {
     print(p_before, digits=3)
     print(p_after, digits=3)
   }
+}
+
+
+# Define, save and remove lm outliers
+# (These were found with 'which(abs(stdres(lm1)) > 5))')
+if (remove_stdres_outliers) {
+  lm_outliers_idx = c(176, 317, 635)
+  lm_outliers = XTrain[lm_outliers_idx,]
+  XTrain = XTrain[-lm_outliers_idx,]
 }
 
 data = XTrain[,c(continuous_cols, factor_cols)]
@@ -70,7 +70,10 @@ vars = vars[-1] # Remove '(Intercept)'
 full_formula = as.formula(paste("~",paste(vars,collapse="+")))
 
 if (transform_response) {
-  response = 'log(co2)'
+  # The power is determined by running:
+  # b = boxcox(model_without_transformed_respons)
+  # lambda = b$x[which.max(b$y)]
+  response = 'co2^-0.3'
 } else {
   response = 'co2'
 }
@@ -83,11 +86,15 @@ lm1 = stepAIC(full_lm, list(lower=~1, upper=full_formula), direction='both')
 empty_model = lm(as.formula(paste(response,'~1')), data_encoded)
 lm2 = stepAIC(empty_model, list(lower=~1, upper=full_formula), direction='both')
 
+# (Extra: model with interaction terms)
+# full2_lm = lm(as.formula(paste(response,'~.^2')), data_encoded)
+# lm1 = stepAIC(full2_lm, list(lower=~1, upper=~.^2), direction='both')
+
 # Check whether the two seeds converged to the same model
 same_vars = setequal(names(lm1$coefficients), names(lm2$coefficients))
 diff = lm1$coefficients[names(data_encoded)] - lm2$coefficients[names(data_encoded)]
 same_coeffs = all((diff < 1e-12) | (is.na(diff)))
-print(same_vars & same_coeffs)
+stepAIC_convergence = same_vars & same_coeffs
 
 print(lm1$anova)
 
@@ -96,39 +103,34 @@ plot_stdres_bounds = function() {
   abline(+2.5, 0, lty=2)
 }
 
-evaluate = function(model) {
-  print(summary(model))
-  standardised_residuals = stdres(model)
-  
-  for (col in c(factor_cols,continuous_cols)) {
-    plot(XTrain[[col]], standardised_residuals, xlab=names(X)[col])
-    plot_stdres_bounds()
-  }
-  
-  plot(standardised_residuals)
+# plot(lm1)
+
+# Evaluate the model
+model = lm1
+print(summary(model))
+standardised_residuals = stdres(model)
+
+for (col in c(factor_cols,continuous_cols)) {
+  plot(XTrain[[col]], standardised_residuals, xlab=names(X)[col])
   plot_stdres_bounds()
-  
-  plot(fitted(model), standardised_residuals)
-  plot_stdres_bounds()
-  
-  boxcox(model)
-  
-  qqnorm(standardised_residuals)
-  qqline(standardised_residuals)
-  shapiro.test(standardised_residuals)
-  
-  print(sum(model$residuals^2))
 }
 
-evaluate(lm1)
-# plot(lm1)
+plot(standardised_residuals)
+plot_stdres_bounds()
+
+plot(fitted(model), standardised_residuals)
+plot_stdres_bounds()
+
+b = boxcox(model)
+lambda = b$x[which.max(b$y)]
+
+qqnorm(standardised_residuals)
+qqline(standardised_residuals)
+shapiro.test(standardised_residuals)
 
 # Compare with robust linear model
 # lmrob = ltsreg(log(co2)~., data)
 # ltsPlot(lmrob, which=c('rfit'))
-
-# Find stdres outliers
-which(abs(stdres(lm1)) > 5)
 
 # Diagnostic plot
 if (!remove_stdres_outliers) {
@@ -136,3 +138,10 @@ if (!remove_stdres_outliers) {
   plot_stdres_bounds()
 }
 
+# Find stdres outliers
+which(abs(stdres(lm1)) > 5)
+
+
+print(sum(model$residuals^2))
+print(lambda, digits=5)
+print(stepAIC_convergence)
