568f2214dcd85e3b0daafeae16503705e91f50ad
tfiers <tomas.fiers@gmail.com>
2017-06-06 15:43:07 +0200

    Table geekery

--- src/pairs.R
+++ src/pairs.R
@@ -5,6 +5,6 @@ library(GGally)
 
 # Pairwise plots
 ggpairs(XTrain[,c(continuous_cols, factor_cols)],
-        aes(alpha=0.4, color=fuel_type),
+        aes(alpha=0.4),
         labeller='label_parsed')
 ggsave('../pairs.pdf', width=15, height=13)
